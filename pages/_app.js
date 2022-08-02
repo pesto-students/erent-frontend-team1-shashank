ab85db296077fac01da8e04e46d7586b5d3e93f2
Ruhan Khandakar <khandakarruhan@gmail.com>
2022-08-02 19:33:36 +0530

    setup: husky for linting and commit messages

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import { StyledThemeProvider } from "@definitions/styled-components";
 
 import "antd/dist/antd.css";
-import "../styles/globals.css";
+import GlobalStyle from "@styles/globalStyles";
 
 function MyApp({ Component, pageProps }) {
     return (
-        <StyledThemeProvider>
-            <Component {...pageProps} />
-        </StyledThemeProvider>
+        <>
+            <GlobalStyle />
+            <StyledThemeProvider>
+                <Component {...pageProps} />
+            </StyledThemeProvider>
+        </>
     );
 }
 
